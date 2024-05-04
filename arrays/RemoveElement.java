fea81e80690e129742c1614799a9fa89cda74542
umesh sharma <umeshsharma07072003@gmail.com>
2024-05-04 17:42:04 +0530

    array problems

---
+++ arrays/RemoveElement.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class RemoveElement {
+	static int[] removeElement(int arr[],int element) {
+		int newArr[] = new int[arr.length-1];
+		
+		for(int i = 0,k=0; i < arr.length; i++) {
+			if(arr[i] == element) {
+				continue;
+			}else {
+				newArr[k++] = arr[i];
+			}
+		}
+		return newArr;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter size of an array");
+		int size = sc.nextInt();
+		
+		int arr[] = new int[size];
+		ArrayMethods.InputUserArray(arr, size);
+		ArrayMethods.printArray(arr);
+		
+		System.out.println("enter element you want to remove");
+		int element = sc.nextInt();
+		
+		int ans[] = removeElement(arr, element);
+		System.out.println("array after removing " + element );
+		ArrayMethods.printArray(ans);
+		
+		
+	}
+}	
