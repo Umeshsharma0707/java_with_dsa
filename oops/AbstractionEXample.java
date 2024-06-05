6d538494df1a466ffb2e90ac6297109d0b0c4bf6
umesh sharma <umeshsharma07072003@gmail.com>
2024-06-05 22:55:01 +0530

    abstract class

---
+++ oops/AbstractionEXample.java
@@ -0,0 +1,34 @@
+package oops;
+
+abstract class Car{
+	
+	int price;
+	
+	abstract void start();
+	
+}
+
+class Audi extends Car{
+
+	@Override
+	void start() {
+		System.out.println("Audi is starting");
+	}
+	
+}
+class BMW extends Car{
+	@Override
+	void start() {
+		System.out.println("BMW is starting");
+	}
+}
+public class AbstractionEXample {
+	public static void main(String[] args) {
+		BMW bmw = new BMW();
+		
+		bmw.start();
+		bmw.price = 5000000;
+		
+		System.out.println(bmw.price);
+	}
+}
