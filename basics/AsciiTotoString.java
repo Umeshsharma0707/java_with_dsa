a437add172c0d121a05aa455dabfdc98123ae74d
umesh sharma <umeshsharma07072003@gmail.com>
2024-06-06 22:45:43 +0530

    practice

---
+++ basics/AsciiTotoString.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class AsciiTotoString {
+	public static void main(String[] args) {
+		int ascii = 99;
+		
+		String ch = Character.toString((char)ascii);
+		System.out.println(ch);
+		
+		
+	}
+}
